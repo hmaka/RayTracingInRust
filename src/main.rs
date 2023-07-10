e73c8a5d36aae062e74261c92082f7f7c421fb0a
Havish <Havish747@gmail.com>
2023-07-09 21:21:42 -0400

    Added ppm file output code to render image

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
 pub mod vec3;
+use vec3::Vec3;
 
 fn main() {
-    println!("Hello, world!");
+    // Image
+    let image_width: i32 = 256;
+    let image_height: i32 = 256;
+
+    // Render
+    println!("P3");
+    println!("{} {}", image_width, image_height);
+    println!("255");
+
+    for j in (0..image_height).rev() {
+        for i in 0..image_width {
+            let pixel_color: Vec3 = Vec3::new(
+                (i as f32) / (image_width - 1) as f32,
+                (j as f32) / (image_height - 1) as f32,
+                0.25f32,
+            );
+            pixel_color.write_color();
+        }
+    }
 }
