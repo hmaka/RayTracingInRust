e4ef8b68fcea6853a7e12870f4ff35bce082627f
Havish <Havish747@gmail.com>
2023-07-13 19:30:41 -0400

    Created trait hittable and companion hit function

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,7 +1,11 @@
-use crate::vec3::Vec3;
+use crate::{ray::Ray, vec3::Vec3};
 
 pub struct HitRecord {
     p: Vec3,
     normal: Vec3,
     t: f32,
+}
+
+trait Hittable {
+    fn hit(r: &Ray, t_min: f32, t_max: f32, rec: &HitRecord);
 }
